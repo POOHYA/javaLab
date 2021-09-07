f48cd859b73c104c50ccf7c0e43e24bd88fd1a14
POOHYA <hyoju2259@gmail.com>
2021-09-07 16:04:22 +0900

    210907

---
+++ chap06Lab/src/sec04/exam01_class_new/Student.java
@@ -0,0 +1,18 @@
+package sec04.exam01_class_new;
+
+//library(API)용 클래스
+//	public은 접근제한자
+//	클래스정의
+public class Student {		
+	/*
+	 * @구성멤버
+	 * 1.필드		객체의 고유 데이터
+	 * 2.생성		객체의 현재 상태 데이터
+	 * 3.메소드	객체가 가져야 할 부품객체
+	 */
+	
+	//기본생성자
+	Student() {
+		
+	}
+}
