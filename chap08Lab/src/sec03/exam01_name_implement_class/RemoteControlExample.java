4f905112a707620bbd827fa75b5c493e04473b7f
POOHYA <hyoju2259@gmail.com>
2021-09-13 13:13:52 +0900

    chap08

---
+++ chap08Lab/src/sec03/exam01_name_implement_class/RemoteControlExample.java
@@ -0,0 +1,10 @@
+package sec03.exam01_name_implement_class;
+
+public class RemoteControlExample {
+	public static void main(String[] args) {
+		//rc:º¯¼ö
+		RemoteControl rc;
+		rc = new Television();
+		rc = new Audio();
+	}
+}
